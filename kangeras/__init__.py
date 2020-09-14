cbdc375db8855fd07cba2f20b28464c4b9d1c78f
jasmcaus <61184127+jasmcaus@users.noreply.github.com>
2020-09-14 13:06:30 +0530

    Added VGG16 architecture to __init_.py

--- kangeras/__init__.py
+++ kangeras/__init__.py
@@ -3,6 +3,7 @@
 
 from .models import createDefaultModel
 from .models import LeNet
+from .models import VGG16
 from .models import saveModel
 from .models import testModel
 from .models import ImageDataGenerator
