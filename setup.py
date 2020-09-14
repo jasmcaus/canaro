5b55a91d3b7c8cffc4ea9fb97c41a46f24dc3073
jasmcaus <61184127+jasmcaus@users.noreply.github.com>
2020-09-14 15:36:36 +0530

    Bug fixes

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ LONG_DESCRIPTION = DESCRIPTION + """ This repository is actively being maintaine
 
 setuptools.setup(
     name="kangeras",
-    version="0.0.6",
+    version="0.0.7",
     author="Jason Dsouza",
     author_email="jasmcaus@gmail.com",
     description=DESCRIPTION,
