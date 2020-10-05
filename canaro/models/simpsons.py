7fc64795c50e4ded7a31fe834fe6a715c0ae1c3d
jasmcaus <61184127+jasmcaus@users.noreply.github.com>
2020-10-05 22:05:52 +0530

    Structural changes to simpsons.py

--- canaro/models/simpsons.py
+++ canaro/models/simpsons.py
@@ -1,14 +1,52 @@
 # # Author: Jason Dsouza
 # # Github: http://www.github.com/jasmcaus
 
-# # Surpressing Tensorflow Warnings
-# import os
-# import time
-# os.environ['TF_CPP_MIN_LOG_LEVEL'] = '2' 
-# # 0 = all messages are logged (default behavior)
-# # 1 = INFO messages are not printed
-# # 2 = INFO and WARNING messages are not printed
-# # 3 = INFO, WARNING, and ERROR messages are not printed
+# Surpressing Tensorflow Warnings
+import os
+os.environ['TF_CPP_MIN_LOG_LEVEL'] = '2' 
+# 0 = all messages are logged (default behavior)
+# 1 = INFO messages are not printed
+# 2 = INFO and WARNING messages are not printed
+# 3 = INFO, WARNING, and ERROR messages are not printed
+
+from tensorflow.keras.models import Model
+from tensorflow.keras.layers import Dense, Flatten, Conv2D, MaxPooling2D, Dropout, Input
+from tensorflow.keras.optimizers import SGD
+
+
+# def createSimpsonsModel(IMG_SIZE=(224,224), channels=1, output_dim=1, loss='binary_crossentropy', decay=None, learning_rate=None, momentum=None, nesterov=None):
+def createSimpsonsModel(IMG_SIZE=(224,224), channels=1, output_dim=1):
+    w,h = IMG_SIZE[:2]
+    input_shape = (w,h,channels)
+    input = Input(input_shape)
+    
+    model = Conv2D(32, (3, 3), padding='same', input_shape=input_shape, activation='relu', name='input_node')(input)
+    model = Conv2D(32, (3, 3), activation='relu')(model)
+    model = MaxPooling2D(pool_size=(2, 2))(model)
+    model = Dropout(0.2)(model)
+
+    model = Conv2D(64, (3, 3), padding='same', activation='relu')(model)
+    model = Conv2D(64, (3, 3), activation='relu')(model)
+    model = MaxPooling2D(pool_size=(2, 2))(model)
+    model = Dropout(0.2)(model)
+
+    model = Conv2D(256, (3, 3), padding='same', activation='relu')(model)
+    model = Conv2D(256, (3, 3), activation='relu')(model)
+    model = MaxPooling2D(pool_size=(2, 2))(model)
+    model = Dropout(0.2)(model)
+
+    model = Flatten()(model)
+    model = Dropout(0.5)(model)
+    model = Dense(1024, activation='relu')(model)
+    
+    # Output Layer
+    output = Dense(output_dim, activation='softmax', name='output_node')(model)
+    
+    model = Model(inputs=input, outputs=output)
+
+    # optimizer = SGD(lr=LEARNING_RATE, decay=DECAY, momentum=MOMENTUM, nesterov=nesterov)
+    # model.compile(loss=loss, optimizer=optimizer, metrics=['accuracy'])
+    return model
 
 # # Importing the necessary packages
 # from tensorflow.keras import backend
@@ -82,43 +120,3 @@
 
 #     model.compile(loss=loss, optimizer=optimizer, metrics=['accuracy'])
 #     return model
-
-
-from tensorflow.keras.models import Model
-from tensorflow.keras.layers import Dense, Flatten, Conv2D, MaxPooling2D, Dropout, Input
-from tensorflow.keras.optimizers import SGD
-
-
-# def createSimpsonsModel(IMG_SIZE=(224,224), channels=1, output_dim=1, loss='binary_crossentropy', decay=None, learning_rate=None, momentum=None, nesterov=None):
-def createSimpsonsModel(IMG_SIZE=(224,224), channels=1, output_dim=1):
-    w,h = IMG_SIZE[:2]
-    input_shape = (w,h,channels)
-    input = Input(input_shape)
-    
-    model = Conv2D(32, (3, 3), padding='same', input_shape=input_shape, activation='relu', name='input_node')(input)
-    model = Conv2D(32, (3, 3), activation='relu')(model)
-    model = MaxPooling2D(pool_size=(2, 2))(model)
-    model = Dropout(0.2)(model)
-
-    model = Conv2D(64, (3, 3), padding='same', activation='relu')(model)
-    model = Conv2D(64, (3, 3), activation='relu')(model)
-    model = MaxPooling2D(pool_size=(2, 2))(model)
-    model = Dropout(0.2)(model)
-
-    model = Conv2D(256, (3, 3), padding='same', activation='relu')(model)
-    model = Conv2D(256, (3, 3), activation='relu')(model)
-    model = MaxPooling2D(pool_size=(2, 2))(model)
-    model = Dropout(0.2)(model)
-
-    model = Flatten()(model)
-    model = Dropout(0.5)(model)
-    model = Dense(1024, activation='relu')(model)
-    
-    # Output Layer
-    output = Dense(output_dim, activation='softmax', name='output_node')(model)
-    
-    model = Model(inputs=input, outputs=output)
-
-    # optimizer = SGD(lr=LEARNING_RATE, decay=DECAY, momentum=MOMENTUM, nesterov=nesterov)
-    # model.compile(loss=loss, optimizer=optimizer, metrics=['accuracy'])
-    return model
